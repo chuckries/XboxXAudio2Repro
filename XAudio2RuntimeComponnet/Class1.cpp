9393338b04d6798d033dfe814de67549efdc1f89
Chuck Ries <chuck.ries@gmail.com>
2016-04-04 23:04:37 -0700

    Initial import of repro projects

---
+++ XAudio2RuntimeComponnet/Class1.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace XAudio2RuntimeComponnet;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
+
+void Class1::CreateXAudio2Stuff()
+{
+    NativeClass::CreateXAudio2Stuff();
+}
+
+void NativeClass::CreateXAudio2Stuff()
+{
+    HRESULT hr = S_OK;
+
+    CComPtr<IXAudio2> pXAudio2;
+    hr = XAudio2Create(&pXAudio2);
+    if (FAILED(hr))
+    {
+        return;
+    }
+
+    CAutoPtr<IXAudio2MasteringVoice> pMasteringVoice;
+    hr = pXAudio2->CreateMasteringVoice(&pMasteringVoice.m_p);
+    if (FAILED(hr))
+    {
+        return;
+    }
+
+    // If you make it here, it worked!
+    __debugbreak();
+}
---
+++ XAudio2RuntimeComponnet/Class1.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+namespace XAudio2RuntimeComponnet
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+        static void CreateXAudio2Stuff();
+    };
+
+    class NativeClass
+    {
+    public:
+        static void CreateXAudio2Stuff();
+    };
+}
